6493c5dadf444b8eaeefb78a7a77167f6832eeb3
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-08 11:39:41 +0700

    waktu habis

--- promise-migration/routes/siswa.js
+++ promise-migration/routes/siswa.js
@@ -1,17 +1,3 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('db/database.db')
-
-class Siswa{
-    
-    static findAll(){
-        let promise = new Promise((resolve, reject)=>{
-            db.all(`SELECT * FROM siswa`, (err, row)=>{
-                if (!err){
-                    resolve(row)
-                } else {
-                    reject(err)
-                }
-            })
-        })
-    }
-}
+const express = require('express')
+const router = express.Router();
+const Siswa = require('./models/siswa')
